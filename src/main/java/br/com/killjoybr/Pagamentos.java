59d9fcef10a9be797f30523fa97fefe58a623af6
killjoybr <112730142+Killjoybr@users.noreply.github.com>
2024-06-09 16:41:09 -0300

    adicionando classe Pagamentos

---
+++ src/main/java/br/com/killjoybr/Pagamentos.java
@@ -0,0 +1,50 @@
+package br.com.killjoybr;
+
+import java.sql.Date;
+
+public class Pagamentos {
+    private int id;
+    private Date data;
+    private double valorTotal;
+    private Produtos[] produtosComprados;
+
+    public Pagamentos(int id, Date data, double valorTotal, Produtos[] produtosComprados) {
+        this.id = id;
+        this.data = data;
+        this.valorTotal = valorTotal;
+        this.produtosComprados = produtosComprados;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    public double getValorTotal() {
+        return valorTotal;
+    }
+
+    public void setValorTotal(double valorTotal) {
+        this.valorTotal = valorTotal;
+    }
+
+    public Produtos[] getProdutosComprados() {
+        return produtosComprados;
+    }
+
+    public void setProdutosComprados(Produtos[] produtosComprados) {
+        this.produtosComprados = produtosComprados;
+    }
+    
+}
